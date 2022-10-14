a4b0fbaa553bae08eca6f4506e3c72929c84acd9
nehuenmorales <nehuenmorales640@gmail.com>
2022-10-14 17:19:14 -0300

    First commit

---
+++ src/com/example/TiposDatos.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class TiposDatos {
+    public static void main(String[] args) {
+
+        //Datos numericos
+        int numero1 = 1;
+        long numero2 = 1283512827;
+        //Decimales
+        float numero3 = 2.2f;
+        double numero4 = 3.33d;
+
+        //booleanos
+        boolean uno = true;
+        boolean dos = false;
+
+
+        // texto
+        char texto1 = 'a';
+        String texto2 = "pepito es un gran personaje de una tira animada";
+
+        //Imprimir todos los datos
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(uno);
+        System.out.println(dos);
+        System.out.println(texto1);
+        System.out.println(texto2);
+
+    }
+}
