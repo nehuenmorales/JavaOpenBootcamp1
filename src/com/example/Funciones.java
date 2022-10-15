8a8bd92c21d2538abe008f21335aa6c416391b9e
nehuenmorales <nehuenmorales640@gmail.com>
2022-10-15 00:16:50 -0300

    funciones

---
+++ src/com/example/Funciones.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Funciones {
+    public static void main(String[] args) {
+
+        int precio = 110;
+        double resultado =  sumaIVA(precio);
+
+        System.out.println(resultado);
+    }
+
+    static double sumaIVA(int precio) {
+        double iva = 12.5;
+        return precio + iva;
+    }
+}
