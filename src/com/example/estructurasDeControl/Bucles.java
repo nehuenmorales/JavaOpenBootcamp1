87c2b19a687b3771235eaa8e377e3efa8a323862
nehuenmorales <nehuenmorales640@gmail.com>
2022-10-15 22:41:53 -0300

    ejersicio Open Bootcamp

--- src/com/example/estructurasDeControl/Bucles.java
+++ src/com/example/estructurasDeControl/Bucles.java
@@ -1,2 +1,23 @@
-package com.example.estructurasDeControl;public class Bucles {
+package com.example.estructurasDeControl;
+
+public class Bucles {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"pepe", "lolo", "carlos", "manuel", "santiago"};
+
+        String resultado = concatenarTexto(nombres);
+        System.out.println(resultado);
+    }
+
+    static String concatenarTexto(String[] nombres){
+
+        String texto = "";
+
+        for(int i = 0; i < nombres.length; i++){
+            texto = texto + nombres[i] + " ";
+        }
+        return texto;
+    }
+
 }
